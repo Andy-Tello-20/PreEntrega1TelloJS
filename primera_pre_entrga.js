2c7260857e8b29090a30f6f530dbe633dd39a9ec
Andres Tello <andrest911@gmail.com>
2023-06-02 15:45:47 -0300

    Cambios en las variables

--- primera_pre_entrga.js
+++ primera_pre_entrga.js
@@ -2,47 +2,47 @@ function confirmarAccion() {
     let respuesta = confirm("Bienvenido al simulador de contabilidad\nDesea continuar?")
 
     if (respuesta) {
-        let continuar=true
-        while(continuar){
-            let vt
-            let gb
+        let continuar = true
+        while (continuar) {
+            let ventasTotales
+            let gananciaBruta
             let interes
             let valor = true
             let intentos = 3
-        
-            //En este bucle (si "vt" no es un numero y los intentos son >=1) seguira ejecutandose a menos de que los intentos == 0, si "vt" es un numero e "intentos" >=1, el bucle se termina
+
+            //En este bucle (si "ventasTotales" no es un numero y los intentos son >=1) seguira ejecutandose a menos de que los intentos == 0, si "ventasTotales" es un numero e "intentos" >=1, el bucle se termina
             while (true) {
-                vt = parseInt(prompt("Comencemos!\nIndique las ventas totales: "))
-                if (isNaN(vt) && intentos >= 1) {
-                intentos -= 1
-                console.log(intentos)
-                alert(`No es un número\nLe quedan ${intentos} intentos`)
+                ventasTotales = parseInt(prompt("Comencemos!\nIndique las ventas totales: "))
+                if (isNaN(ventasTotales) && intentos >= 1) {
+                    intentos -= 1
+                    console.log(intentos)
+                    alert(`No es un número\nLe quedan ${intentos} intentos`)
                 } else {
-                break
+                    break
                 }
-                
-                if(intentos==0){
+
+                if (intentos == 0) {
                     alert("Imposible continuar")
                     break
-                }else{
+                } else {
 
                 }
             }
 
-            
+
 
             //si los intentos >=1 se ejecuta todo este bloque if
-            if(intentos>=1){
+            if (intentos >= 1) {
 
                 let q1 = parseInt(prompt("Que desea calcular:\n1) Porcentaje de ganancia\n2) Ganancia segun interes"))
 
 
                 //Este bucle no se termina hasta que "q1" sea igual a 1 o 2 o >1           
-                while (q1 != 1 && q1 != 2 && intentos >1) {
+                while (q1 != 1 && q1 != 2 && intentos > 1) {
 
                     intentos -= 1
 
-                    alert(`Opcion incorrecta. Intente nuevamente\nLe queda ${intentos } intento/s`)
+                    alert(`Opcion incorrecta. Intente nuevamente\nLe queda ${intentos} intento/s`)
 
 
                     q1 = parseInt(prompt("Que desea calcular:\n1) Porcentaje de ganancia\n2) Ganancia segun interes"))
@@ -54,10 +54,10 @@ function confirmarAccion() {
                 if (q1 == 1) {
 
 
-                    //do-while controla que "gb" sea un numero y los intentos >1 .se termina el bucle cambiando valor=false
+                    //do-while controla que "gananciaBruta" sea un numero y los intentos >1 .se termina el bucle cambiando valor=false
                     do {
-                        gb = parseInt(prompt("Comencemos!\nIndique la Ganancia bruta: "))
-                        if (isNaN(gb) && intentos > 1) {
+                        gananciaBruta = parseInt(prompt("Comencemos!\nIndique la Ganancia bruta: "))
+                        if (isNaN(gananciaBruta) && intentos > 1) {
                             intentos -= 1
                             alert(`No es un número\nLe quedan ${intentos} intentos`)
                             valor = true
@@ -68,17 +68,17 @@ function confirmarAccion() {
 
                     } while (valor)
 
-                    if(isNaN(gb)){
+                    if (isNaN(gananciaBruta)) {
                         alert("No se pudo realizar la operacion")
-                    }else{
-                        let porcentaje = (gb * 100) / vt
-                    alert(`Porcentaje de ganancia= ${porcentaje.toFixed(2)}%`)
+                    } else {
+                        let porcentaje = (gananciaBruta * 100) / ventasTotales
+                        alert(`Porcentaje de ganancia= ${porcentaje.toFixed(2)}%`)
 
-                    let costoDeVenta = vt - gb
+                        let costoDeVenta = ventasTotales - gananciaBruta
                         alert(`El costo de venta es $ ${costoDeVenta}`)
                     }
 
-                    
+
 
 
                 } else if (q1 == 2) {
@@ -101,25 +101,25 @@ function confirmarAccion() {
                         alert("No se pudo realizar la operacion")
 
                     } else {
-                        gb = (vt * (interes / 100))
-                        alert(`Ganancia Bruta= $${gb}`)
+                        gananciaBruta = (ventasTotales * (interes / 100))
+                        alert(`Ganancia Bruta= $${gananciaBruta}`)
 
-                        let costoDeVenta = vt - gb
+                        let costoDeVenta = ventasTotales - gananciaBruta
                         alert(`El costo de venta es $${costoDeVenta}`)
                     }
 
 
 
-                }else {
+                } else {
                     alert("No tiene mas intentos.")
                 }
 
-            }else{
+            } else {
             }
 
-            let pregunta=prompt("Desea comenzar nuevamente?\nIngrese 'si' o 'no'")
+            let pregunta = prompt("Desea comenzar nuevamente?\nIngrese 'si' o 'no'")
 
-            let repeticion=pregunta.toLowerCase()
+            let repeticion = pregunta.toLowerCase()
 
 
             //En este switch las opciones son solamente 2 las disponibles
@@ -129,19 +129,19 @@ function confirmarAccion() {
                 case "si":
                     break
                 case "no":
-                  continuar=false
-                  alert("Hasta pronto!!")
-                  break
+                    continuar = false
+                    alert("Hasta pronto!!")
+                    break
                 default:
                     alert("Opcion incorrecta")
-                    continuar=false
+                    continuar = false
                     break
-                
-              }
+
+            }
 
 
         }
-        
+
 
     } else {
         document.write("Acción cancelada.")
